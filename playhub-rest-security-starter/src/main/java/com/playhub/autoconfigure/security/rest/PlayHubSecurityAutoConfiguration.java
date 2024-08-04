1456905e7ffa9df2f291acb3c8d54bc9c34caf37
Скорик Артур Андреевич <skorik.aa@esphere.ru>
2024-08-04 21:18:38 +0400

    fixed security starter config

--- playhub-rest-security-starter/src/main/java/com/playhub/autoconfigure/security/rest/PlayHubSecurityAutoConfiguration.java
+++ playhub-rest-security-starter/src/main/java/com/playhub/autoconfigure/security/rest/PlayHubSecurityAutoConfiguration.java
@@ -5,17 +5,20 @@ import com.playhub.autoconfigure.security.rest.jwt.DefaultJwdDecoder;
 import com.playhub.autoconfigure.security.rest.jwt.PlayHubJwtAuthenticationConverter;
 import com.playhub.autoconfigure.security.rest.jwt.PlayHubJwtGrantedAuthoritiesConverter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
-@Configuration
+@AutoConfiguration
 @Slf4j
 public class PlayHubSecurityAutoConfiguration {
 
@@ -26,12 +29,18 @@ public class PlayHubSecurityAutoConfiguration {
                 customizer.jwt(jwtCustomizer ->
                         jwtCustomizer.jwtAuthenticationConverter(jwtAuthenticationConverter())
                 ));
-        http.authorizeHttpRequests(registry -> registry.anyRequest().authenticated())
+        return http
+                .authorizeHttpRequests(registry -> registry.anyRequest().authenticated())
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable);
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ehc ->
+                        ehc.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .build();
 
-        return http.build();
+
     }
 
     @Bean
